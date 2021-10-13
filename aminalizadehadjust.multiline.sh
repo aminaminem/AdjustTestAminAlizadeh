2843ea08dde0a7c65eac45a230772850e5162ee5
Amin Alizadeh <92444332+aminaminem@users.noreply.github.com>
2021-10-13 19:29:33 +0200

    Add files via upload

---
+++ aminalizadehadjust.multiline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This is a bash test personal project by amin alizadeh @ 20211013
+#contact : aminaminem@gmail.com
+#Project is about design a script that writes the numbers from 1 - 10 in random order and Each number should appear only once.
+#This file is responsible for give the numbers in multi lines
+
+shuf -i 1-10 -n 10
